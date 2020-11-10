96641967cb8149a6db880a7750a457766bcfb0b7
Valery Stolbov <valera.stolbov.2000@mail.ru>
2020-11-10 22:57:41 +0200

    Исправления

--- lession.php
+++ lession.php
@@ -2,25 +2,29 @@
 
 require_once "function.php";
 
-function assertEquals($expected, $received, $message){
-    if ($expected == $received)
+function assertEquals($expected, $received, $message)
+{
+    if ($expected === $received)
     {
-        echo $message." - passed\n";
+        echo $message." - passed".PHP_EOL;
     }
     else
     {
-        echo $message." - failed\n";
+        echo $message." - failed".PHP_EOL;
     }
 }
 
-echo "Проверка функции Read_from_console на правильность исполнения\n";
-echo "В конце программы можно проверить корректность ввода через консоль\n";
+echo "Проверка функции Read_from_console на правильность исполнения".PHP_EOL;
+echo "В конце программы можно проверить корректность ввода через консоль".PHP_EOL;
 
 $result = readFromConsole("true");
 assertEquals(true,$result,"test: true");
 
 $result = readFromConsole("false");
 assertEquals(false,$result,'test: false');
+
+$result = readFromConsole("1");
+assertEquals(1,$result,"test: 1");
 
 $result = readFromConsole("2");
 assertEquals(2,$result,"test: 2");
