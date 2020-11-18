9ada6d3cf171ee9992457baa8ead8e7a36f86d83
Valery Stolbov <valera.stolbov.2000@mail.ru>
2020-11-18 12:05:07 +0200

    Домашняя работа по верстке странички

---
+++ Mockup/main.php
@@ -0,0 +1,52 @@
+<!DOCKTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Страничка</title>
+</head>
+<body>
+<div class="section bg">
+    <div id="block1">
+        <h2>Long headline for two lines for attention and evolving</h2>
+        <p class="pstyle1">
+            With the new AirPlay 2, you can control your home audio system and the speakers throughout your house.4 You can play a song in the living room and your kitchen at the same time.
+        </p>
+        <div class="wrapper">
+            <img src="Logo1.svg" alt="PayPal" title="PayPal">
+            <img src="Logo2.svg" alt="AndroidPay" title="AndroidPay">
+            <img src="Logo3.svg" alt="ApplePay" title="ApplePay">
+            <img src="Logo4.svg" alt="SamsungPay" title="SamsungPay">
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="wrapper">
+        <div id="block2">
+            <h1>Medium length display headline</h1>
+            <p class="pstyle1">Separated they live in Bookmarks right at the coast of the famous Semantics, large language ocean Separated they live in Bookmarks right
+            </p>
+            <a href="https://www.bitrix24.ru" target="_blank">Action link</a>
+        </div>
+        <img class="picture" src="img5.png" alt="pic">
+    </div>
+</div>
+<div class="section">
+    <div id="block3" class="pstyle2">
+        <h3>Medium Length Title</h3>
+        <h4>Medium length headline</h4>
+        <p>By creating an Account on our service, you agree to <span class="blue">subscribe to newsletters</span>, marketing or promotional materials and other information we may send. However, you may opt out of receiving any, or all, of these communications from us by following the unsubscribe link or instructions provided in any email we send.</p>
+        <h4>Medium length headline</h4>
+        <p>However, you may opt out of receiving any, or all, of these communications from us by following the unsubscribe link or instructions provided in any email we send.</p>
+        <p>By creating an Account on our service, you agree to subscribe to newsletters, marketing or promotional materials and other information we may send. However, you may opt out of receiving any, or all, of these communications from us by following the unsubscribe link or instructions provided in any email we send. You understand that your content (<span class="blue">not including credit card information</span>), may be transferred unencrypted and involve (a) transmissions over various networks; and (b) changes to conform and adapt to technical requirements of connecting networks or devices. Credit card information is always encrypted during transfer over networks.</p>
+        <ul>
+            <li>Showcase and embed your work with</li>
+            <li>Publish across social channels in a click</li>
+            <li>Sell your videos worldwide</li>
+            <li>Embed your work with</li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
